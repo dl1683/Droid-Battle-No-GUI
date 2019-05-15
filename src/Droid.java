ad96a98288963e58fbfc656aa78470e3f2d2ca51
DL1683 <dl1683@rit.edu>
2019-05-15 15:50:06 -0400

    Got the thing started

---
+++ src/Droid.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+class Droid{
+    int health;
+    int x;
+    int y;
+    int damage;
+    String team;
+    int range;
+    String name;
+    public Droid(int health,int x,int y,int damage,String team,int range,String name;){
+
+    }
+    public static void main(String [] args){
+        System.out.println("HEllo");
+    }
+}
